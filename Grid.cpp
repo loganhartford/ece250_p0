6ee31f9c69c9ee5ec653b4346c12159e704bf543
loganhartford <logan.hartford@outlook.com>
2024-09-16 11:29:16 -0400

    more tests

--- Grid.cpp
+++ Grid.cpp
@@ -53,7 +53,7 @@ void Grid::clear(void)
 
 bool Grid::isValid(int x, int y)
 {
-    return ((x <= cols) && (y <= rows));
+    return ((x < cols) && (y < rows));
 }
 
 double Grid::move(int x, int y)
