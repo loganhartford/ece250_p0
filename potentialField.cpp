c1e52e48704f519aab2db2660cfd5760327611f2
loganhartford <logan.hartford@outlook.com>
2024-09-16 09:07:53 -0400

    base parsing

--- potentialField.cpp
+++ potentialField.cpp
@@ -5,11 +5,54 @@ using namespace std;
 int main()
 {
     string cmd;
+    char type;
+    int x;
+    int y;
+    double k;
     while (cin >> cmd)
     {
         if (cmd == "CREATE")
         {
             cout << "success" << endl;
+        }
+        else if (cmd == "POINT")
+        {
+            cin >> type;
+            cin >> x;
+            cin >> y;
+            if (type == 'O')
+            {
+            }
+            else
+            {
+            }
+
+            cout << "success" << endl;
+        }
+        else if (cmd == "MOVE")
+        {
+            cin >> x;
+            cin >> y;
+        }
+        else if (cmd == "CLEAR")
+        {
+        }
+        else if (cmd == "UPDATE")
+        {
+            cin >> k;
+            if (k > 0)
+            {
+                cout << "success" << endl;
+            }
+            else
+            {
+                cout << "failure" << endl;
+            }
+        }
+        else if (cmd == "EXIT")
+        {
+            // Dellocate memory
+            return 0;
         }
     }
 
