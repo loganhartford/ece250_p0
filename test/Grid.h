f9d6485ae81be7808daf2a2f3957c7491fe8b800
Logan Hartford <laphartf@uwaterloo.ca>
2024-09-23 18:49:42 -0400

    tar

---
+++ test/Grid.h
@@ -0,0 +1,33 @@
+#ifndef GRID_H
+#define GRID_H
+
+#include <cmath>
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Grid
+{
+public:
+    Grid(int rows, int cols);
+    ~Grid();
+
+    void clear();
+    void setObject(int x, int y, char obj);
+    bool isValid(int x, int y);
+    double move(int x, int y);
+    void updateK(double k);
+
+private:
+    int rows;
+    int cols;
+    double **potentials;
+    char **objects;
+    double k;
+
+    void calcObjPotential(int xO, int yO, char obj);
+    void calcAllPotentials();
+};
+
+#endif // GRID_H
